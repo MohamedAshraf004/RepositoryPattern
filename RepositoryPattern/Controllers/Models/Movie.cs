9224febcd1afcbcda03f3f3bde4254bd67bca17e
Mohamed Ashraf Mohamed Ahmed <LionelMido@std.mans.edu.eg>
2021-03-02 23:25:45 +0200

    Add project files.

---
+++ RepositoryPattern/Controllers/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using RepositoryPattern.Controllers.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Controllers.Models
+{
+    public class Movie : IEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        //Start with capital char and don't contain numbers or space
+        public string Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
